72f34734462beb78d4d1ca22974c7e1dd9c2b5c3
Shrikant Dev <shrikant9907@gmail.com>
2021-01-07 00:00:26 +0530

    initial setup

---
+++ wp-content/themes/course/templates/template-full-width.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template Name: Full Width Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage My_Course
+ * @since My Course 1.0
+ */
+
+get_template_part( 'singular' );
